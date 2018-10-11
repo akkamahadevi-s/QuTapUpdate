bc8b869146d78588faa416491766e37574aa4b2f
akkamahadevi <akkamahadevi@10.2.0.132>
2018-10-11 11:05:14 +0530

    commited by rani

---
+++ https:/github.com/akkamahadevi-santosh/QuTapUpdate.git/QutapApplicationLatest/src/main/java/com/exilant/domain/TestScenario.java
@@ -0,0 +1,53 @@
+package com.exilant.domain;
+
+import java.util.ArrayList;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="testScenario")
+public class TestScenario {
+	
+	private String moduleId; 
+	@Id
+	private String testScenId;
+	private String testScenName;
+	private String testScenDesc;
+	private ArrayList<TestCase> testCaseList;
+	
+	public ArrayList<TestCase> getTestCaseList() {
+		return testCaseList;
+	}
+	public void setTestCaseList(ArrayList<TestCase> testCaseList) {
+		this.testCaseList = testCaseList;
+	}
+	public String getTestScenId() {
+		return testScenId;
+	}
+	public void setTestScenId(String testScenId) {
+		this.testScenId = testScenId;
+	}
+	public String getTestScenName() {
+		return testScenName;
+	}
+	public void setTestScenName(String testScenName) {
+		this.testScenName = testScenName;
+	}
+	public String getTestScenDesc() {
+		return testScenDesc;
+	}
+	public void setTestScenDesc(String testScenDesc) {
+		this.testScenDesc = testScenDesc;
+	}
+	
+	public String getModuleId() {
+		return moduleId;
+	}
+	public void setModuleId(String moduleId) {
+		this.moduleId = moduleId;
+	}
+	
+	
+	
+	
+}
